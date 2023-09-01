63cfd0bf9d5dc8e94413ab4e4f0f7c1b7257662e
a-down <136649391+a2downing@users.noreply.github.com>
2023-09-01 09:20:45 -0500

    about and projects section formatting

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,7 +8,7 @@ export default function About() {
     <>
       <div className=" flex flex-col justify-center gap-16 w-100 bg-accent p-24 items-center">
 
-        <div className='flex justify-between w-full'>
+        <div className='flex justify-between w-full max-w-[974px]'>
           <div className="font-serif max-w-[800px]">
             <p className='p-4 pt-0'>I am a Front End Developer with full-stack experience. My goal is to build websites that are easy to view, understand, and navigate. As a former teacher, I have used everything from beautiful creative websites to outdated software. Confusing websites either slow down the user or just get ignored. Accessible websites make for a better internet. </p>
             <p className='p-4'>I specialize in JavaScript and React while using Tailwind, Bootstrap, Bulma, Chakra, and GitHub. I also have experience with Node.js, Express.js, MySQL, Sequelize, MongoDB, Mongoose, and Handlebars.</p>
@@ -18,7 +18,7 @@ export default function About() {
         </div>
 
         {/* <LanguageDisplay /> */}
-        <div className='w-full bg-slate-600 h-[62px]'>
+        <div className='w-full bg-slate-600 h-[62px] max-w-[974px]'>
 
         </div>
       </div>
--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -4,9 +4,16 @@ import safePawsBrowser from '../../assets/project-visuals/safe-paws-browser.png'
 
 export default function Project() {
 
+  // title
+  // color
+  // phoneImg
+  // browserImg
+  // Repo Link
+  // Deployed Link
+  // techStack: ['asdf', 'asdf', 'asdf']
 
   return (
-    <div className="mx-auto w-[974px] flex flex-col items-center pt-24 gap-10">
+    <div className="mx-auto flex flex-col items-center pt-24 px-24 gap-10">
       <div className='text-center'>
         <h2 className='text-7xl text-safePaws'>Safe Paws</h2>
         <div className="text-gray-200 font-serif text-xl font-thin">
@@ -14,14 +21,14 @@ export default function Project() {
         </div>
       </div>
 
-      <div className=' flex w-[974px] h-[320px] space-between justify-between'>
-        <img src={safePawsPhone} width='147px' className='content-fit'/>
-        <img src={safePawsBrowser} width='491px'/>
-        <p className='w-[255px] bg-slate-200'>Hello</p>
+      <div className=' flex w-full h-[320px] max-w-[974px] space-between justify-between'>
+        <img src={safePawsPhone} width='147px' className='content-fit shadow-md shadow-safePaws'/>
+        <img src={safePawsBrowser} width='491px' className='shadow-md shadow-safePaws'/>
+        <p className='w-[255px] bg-slate-50 shadow-md shadow-safePaws'>Hello</p>
       </div>
 
       {/* <LanguageDisplay /> */}
-      <div className='w-full bg-slate-600 h-[62px]'>
+      <div className='w-[974px] bg-slate-600 h-[62px]'>
 
       </div>
     </div>
