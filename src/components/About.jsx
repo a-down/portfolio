7557572f447ee100e4f87626c1a8835e1aa0f094
a-down <136649391+a2downing@users.noreply.github.com>
2023-09-04 09:21:52 -0500

    about updates

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ import StackDisplay from './StackDisplay/StackDisplay'
 
 export default function About() {
 
-  const myTechStack = ['React', 'JavaScript', 'HTML', 'CSS', 'Tailwind', 'Bootstrap', 'Chakra', 'jQuery', 'GitHub', 'Node.js', 'Express', 'MongoDB']
+  const myTechStack = ['React', 'JavaScript', 'Tailwind', 'Chakra', 'HTML', 'CSS', 'Bootstrap', 'jQuery', 'GitHub', 'Node.js', 'Express', 'MongoDB']
 
   return (
     <>
@@ -14,7 +14,7 @@ export default function About() {
           <div className="font-serif max-w-[800px]">
             <p className='p-4 pt-0 font-sans text-2xl'>I am a Front End Developer</p>
             <p className='p-4 pt-0'>with full-stack experience. My goal is to build websites that are easy to view, understand, and navigate. As a former teacher, I have used everything from beautiful creative websites to outdated software. Confusing websites either slow down the user or just get ignored. Accessible websites make for a better internet. </p>
-            <p className='p-4'>I specialize in JavaScript and React while using Tailwind, Bootstrap, Bulma, Chakra, and GitHub. I also have experience with Node.js, Express.js, MySQL, Sequelize, MongoDB, Mongoose, and Handlebars.</p>
+            <p className='p-4'>I specialize in React and JavaScript while using Tailwind, Bootstrap, Bulma, Chakra, and GitHub. I also have experience with Node.js, Express.js, MySQL, Sequelize, MongoDB, Mongoose, and Handlebars.</p>
           </div>
 
           <img src={headshot} className='w-[250px] h-[250px] rounded-2xl shadow-lg'/>
