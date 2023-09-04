44629578bbc4d8341d01b799c0d6b95a412afba7
a-down <136649391+a2downing@users.noreply.github.com>
2023-09-04 12:41:37 -0500

    projects update

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -16,7 +16,7 @@ export default function Project({ project, stackArr, isDark }) {
   }
 
   return (
-    <div className="mx-auto flex flex-col items-center lg:pt-24 pt-14 px-4 md:px-10 lg:px-24 gap-10">
+    <div className="flex flex-col items-center lg:pt-24 md:pt-16 pt-14 px-8 md:px-10 lg:px-24 gap-10">
       <div className='text-center select-none'>
         <h2 className='text-7xl' style={{color: `${project.color}`}}>{project.title}</h2>
         <div className="text-gray-200 font-serif text-xl font-thin">
