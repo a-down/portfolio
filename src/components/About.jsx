d9b985529681fd0f00c0941fc091a4b13c178ae2
a-down <136649391+a2downing@users.noreply.github.com>
2023-09-04 10:08:20 -0500

    preresponsive design

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,8 +13,8 @@ export default function About() {
         <div className='flex justify-between w-full max-w-[974px]'>
           <div className="font-serif max-w-[800px]">
             <p className='p-4 pt-0 font-sans text-2xl'>I am a Front End Developer</p>
-            <p className='p-4 pt-0'>with full-stack experience. My goal is to build websites that are easy to view, understand, and navigate. As a former teacher, I have used everything from beautiful creative websites to outdated software. Confusing websites either slow down the user or just get ignored. Accessible websites make for a better internet. </p>
-            <p className='p-4'>I specialize in React and JavaScript while using Tailwind, Bootstrap, Bulma, Chakra, and GitHub. I also have experience with Node.js, Express.js, MySQL, Sequelize, MongoDB, Mongoose, and Handlebars.</p>
+            <p className='p-4 pt-0'>with full-stack experience. My goal is to build websites that are easy to view, understand, and navigate. As a former teacher, I have used everything from beautiful creative websites to outdated software. Confusing websites either impede the user or get ignored. Accessible websites make for a better internet. </p>
+            <p className='p-4'>I specialize in React and JavaScript while using Tailwind, Chakra, Bootstrap, and GitHub. I also have experience with Node.js, Express.js, MySQL, Sequelize, MongoDB, Mongoose, and Handlebars.</p>
           </div>
 
           <img src={headshot} className='w-[250px] h-[250px] rounded-2xl shadow-lg'/>
--- src/components/Projects/ProjectsSection.jsx
+++ src/components/Projects/ProjectsSection.jsx
@@ -30,8 +30,8 @@ export default function ProjectSection() {
       browserImg: safePawsBrowser,
       repoLink: 'https://github.com/a-down/safe-paws',
       // deployedLink: 'https://safe-paws-da967720d734.herokuapp.com/',      cardOrder: [1, 2, 3],
-      description: 'Safe Paws was a team project that was completed during my Full Stack Certificate Program. We built a website that used server-side rendering (Handlebars) to dynamically deliver the webpage. Visitors to the website can create an account, add and remove pets, and book appointments.',
-      description2: 'I was the front end builder and designer (HTML and CSS), and I also assisted on the backend as needed. I managed the GitHub Repo, resolved conflicts, and approved pull requests.',
+      description: 'Safe Paws was a team project during my Full-Stack Certificate Program. We built a website that used server-side rendering (Handlebars) to dynamically render the webpage according the the user. Users can create an account, add and remove pets, and book appointments.',
+      description2: 'I was the front-end architect and designer (HTML and CSS). I also assisted on the client-side JavaScript and the backend. I managed the GitHub Repo, resolved merge conflicts, and approved pull requests.',
       techStack: ['HTML', 'CSS', 'JavaScript', 'Bootstrap', 'Handlebars',  'Node.js', 'Express', 'MySQL', 'Sequelize', 'GitHub', 'Heroku']
     },
     {
@@ -42,8 +42,8 @@ export default function ProjectSection() {
       // cardOrder: [3, 2, 1],
       repoLink: 'https://github.com/a-down/tech-blog',
       deployedLink: 'https://a-down-tech-blog-b6cbf85bb984.herokuapp.com',
-      description: 'Undefined Options is a full stack web application that allows users to view blog posts posted on the site. Users can also create an account, post and edit their own blog posts, and comment on posts made by others.',
-      description2: 'The blog site uses server-side rendering (Handlebars) to dynamically update the site based on posts in the SQL database and based on the logged-in user. ',
+      description: 'Undefined Opinions is a full-stack website that allows users to view blog posts posted on the site. Users can also create an account, post and edit their own blog posts, and comment on posts made by others. Users must be signed in to add posts or comments to the MySQL database.',
+      description2: 'The blog side uses server-side rendering (Handlebars) to update the site based on database data and the logged-in user.',
       techStack: ['HTML', 'CSS', 'JavaScript', 'Bulma', 'Handlebars', 'jQuery', 'Express', 'Node.js', 'MySQL', 'Sequelize', 'GitHub', 'Heroku']
     },
     {
@@ -54,8 +54,8 @@ export default function ProjectSection() {
       // cardOrder: [2, 1, 3],
       repoLink: 'https://github.com/a-down/GameVerseHub',
       deployedLink: 'https://a-down.github.io/GameVerseHub/',
-      description: 'GameVerseHub was a team project that was completed during my Full Stack Certificate Program. The site was built to allow users to see featured video games, as we as search for video games by keyword. This site is powered by the RAWG API.',
-      description2: 'I was responsible for client-side JavaScript on the search page. I managed the GitHub Repo, resolved conflicts, and approved pull requests.',
+      description: 'GameVerseHub was a team project during my Full Stack Certificate Program. We built the site for users to see featured video games and search for games by keyword. RAWG API powers the site.',
+      description2: 'I built the client-side JavaScript for the search page. I managed the GitHub Repo, resolved merge conflicts, and approved pull requests.',
       techStack: ['HTML', 'CSS', 'JavaScript', 'jQuery', 'Bootstrap','GitHub' ]
     },
     {
@@ -66,7 +66,7 @@ export default function ProjectSection() {
       // cardOrder: [1, 2, 3],
       repoLink: 'https://github.com/a-down/weather-dashboard',
       deployedLink: 'https://a-down.github.io/weather-dashboard/',
-      description: 'The Weather Dashboard was created for users to search for current and future weather by city names using the OpenWeather API. The site saves user searches so they are easy to search again in the future. The site was built with jQuery.',
+      description: 'The Weather Dashboard allows users to search for current and future weather by city name. The site saves user search history for future reference. The Weather Dashboard is built with jQuery and powered by the OpenWeather API.',
       description2: '',
       techStack: ['HTML', 'CSS', 'JavaScript', 'jQuery', 'GitHub']
     },
@@ -78,7 +78,7 @@ export default function ProjectSection() {
       // cardOrder: [2, 3, 1],
       repoLink: 'https://github.com/a-down/code-quiz',
       deployedLink: 'https://a-down.github.io/code-quiz/',
-      description: 'The Coding Quiz allows users to take a 10-question test on JavaScript knowledge. Users are given 90 seconds to answer every question. At the end of the quiz, users can save their initials and score to local storage.',
+      description: 'The Coding Quiz allows users to take a 10-question test on JavaScript. Users have 90 seconds to answer every question. Users can save their initials and scores to compare to previous and future attempts.',
       description2: '',
       techStack: ['HTML', 'CSS', 'JavaScript', 'GitHub']
     },
@@ -90,8 +90,8 @@ export default function ProjectSection() {
       // cardOrder: [3, 2, 1],
       repoLink: 'https://github.com/a-down/note-taker',
       deployedLink: 'https://a-down-note-taker-a4f5d9c37c25.herokuapp.com/',
-      description: 'Note Taker is a full-stack website where users can save notes and stay organized. When a note is created, Express and Node.js are used to save the note as a JSON file in the server. Users can also delete notes saved to the site.',
-      description2: 'I was responsible for building out the backend of the website and deploying the website.',
+      description: 'Note Taker is a full-stack website for users to save notes to stay organized. Express.js and Node.js save the notes as JSON files in the server. Users can also delete notes saved to the site.',
+      description2: 'I built out the backend of the website and deployed the website to Heroku.',
       techStack: ['HTML', 'CSS', 'JavaScript', 'Express', 'Node.js', 'GitHub', 'Heroku']
     },
   ]
