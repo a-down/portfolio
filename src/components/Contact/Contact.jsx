1390598cf837a27a7ad7e216252ee786c1ada9f8
a-down <136649391+a2downing@users.noreply.github.com>
2023-09-05 09:34:50 -0500

    contact links update

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -2,6 +2,7 @@ import ContactForm from './ContactForm'
 import { SiGithub, SiLinkedin } from 'react-icons/si'
 import { HiMiniDocumentText } from 'react-icons/hi2'
 import { HiOutlineMail } from 'react-icons/hi'
+import resume from '../../assets/alec-downing-resume.pdf'
 
 
 
@@ -17,22 +18,22 @@ export default function Contact(){
         <div className='flex flex-col md:flex-row gap-10 justify-between text-xl font-serif w-full py-10'>
 
           <div className='flex flex-col gap-8 order-1 md:-order-1'>
-            <a href='' className='flex items-center gap-4 hover:text-slate-600'>
+            <a href='https://github.com/a-down' target='_blank' className='flex items-center gap-4 hover:text-slate-600 active:text-slate-200'>
               <SiGithub />
               <h4>a-down</h4>
             </a>
 
-            <a href='' className='flex items-center gap-4 hover:text-slate-600'>
+            <a href='https://linkedin/in/alec-downing/' target='_blank' className='flex items-center gap-4 hover:text-slate-600 active:text-slate-200'>
               <SiLinkedin />
               <h4>alec-downing</h4>
             </a>
 
-            <a href='' className='flex items-center gap-4 hover:text-slate-600'>
+            <a href="mailto: alec2downing@gmail.com" className='flex items-center gap-4 hover:text-slate-600 active:text-slate-200'>
               <HiOutlineMail />
               <h4>alec2downing@gmail.com</h4>
             </a>
 
-            <a href='' className='flex items-center gap-4 hover:text-slate-600'>
+            <a href={resume} download='alec-downing-resume' className='flex items-center gap-4 hover:text-slate-600 active:text-slate-200'>
               <HiMiniDocumentText />
               <h4>Resume</h4>
             </a>
