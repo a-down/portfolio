2937ee6826d5bfbdd25c908a3fc67eebace7930d
a-down <136649391+a2downing@users.noreply.github.com>
2023-09-21 13:47:37 -0500

    bio update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -21,7 +21,7 @@ export default function About() {
             <div className="font-serif max-w-[800px]">
               <p className='p-4 pt-0 font-sans text-2xl'>I am a Front End Developer</p>
               <p className='p-4 pt-0 text-justify'>with full-stack experience. My goal is to build websites that are easy to view, understand, and navigate. As a former teacher, I have used everything from beautiful creative websites to outdated software. Confusing websites either impede the user or get ignored. Accessible websites make for a better internet. </p>
-              <p className='p-4 text-justify'>I specialize in React and JavaScript while using Tailwind, Chakra, Bootstrap, and GitHub. I also have experience with Node.js, Express.js, MySQL, Sequelize, MongoDB, Mongoose, and Handlebars.</p>
+              <p className='p-4 text-justify'>I specialize in React and JavaScript while using Tailwind, Radix UI, Chakra, Bootstrap, and GitHub. I also have experience with Node.js, Express.js, MySQL, Sequelize, MongoDB, Mongoose, and Handlebars.</p>
             </div>
 
             <img src={headshot} className='w-[250px] h-[250px] rounded-2xl shadow-lg -order-1 md:order-3 self-center m-4 md:m-0'/>
