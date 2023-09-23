75e0ee47f0a5e1a5f9dcdc6a51a26e7c2aacce25
a-down <136649391+a2downing@users.noreply.github.com>
2023-09-23 16:02:40 -0500

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,7 +10,7 @@ export default function HeroText(){
 
       <h1 className=' text-5xl p-2'>Alec Downing</h1>
       <img src={lineGraphic} className='p-2 select-none'/>
-      <h3 className=' text-3xl font-serif font-thin'>Front End Developer</h3>
+      <h3 className=' text-3xl font-serif font-thin'>Frontend Developer</h3>
       <h4 className=' text-xl font-serif font-thin'>Making the Internet Easier to Use</h4>
 
       <div className='flex mt-52 gap-4 w-full justify-center md:justify-end md:px-10 lg:px-24'>
