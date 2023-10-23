837564f20655718306fcc8852b722055ca2d968c
a-down <136649391+a2downing@users.noreply.github.com>
2023-10-23 12:27:28 -0500

    bio updates

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -19,8 +19,8 @@ export default function About() {
           <div className='flex flex-col md:flex-row justify-between w-full max-w-[974px]'>
             <div className="font-serif max-w-[800px]">
               <p className='p-4 pt-0 font-sans text-2xl'>I am a Frontend Developer</p>
-              <p className='p-4 pt-0 text-justify'>with full-stack experience. My goal is to build websites that are easy to view, understand, and navigate. As a former teacher, I have used everything from beautiful creative websites to outdated software. Confusing websites either impede the user or get ignored. Accessible websites make for a better internet. </p>
-              <p className='p-4 text-justify'>I specialize in React and JavaScript while using Tailwind, Radix UI, Chakra, Bootstrap, and GitHub. I also have experience with Node.js, Express.js, MySQL, Sequelize, MongoDB, Mongoose, and Handlebars.</p>
+              <p className='p-4 pt-0 text-justify'>focused on React, NextJS, JavaScript, and Tailwind. I am also comfortable using jQuery, various CSS Frameworks (Bootstrap, Material UI, Radix UI, Chakra, Bulma), and GitHub. I have full-stack experience with NodeJS, ExpressJS, MongoDB, Mongoose, MySQL, Sequelize, JWT, and Clerk.</p>
+              <p className='p-4 text-justify'>I build websites that are easy to view, understand, and navigate. I will collaborate and communicate with designers and developers to create user-focused experiences. As a former teacher, I have used everything from beautiful creative websites to outdated software. Confusing websites either impede the user or get ignored. Accessible websites make for a better internet!</p>
             </div>
 
             <img src={headshot} className='w-[250px] h-[250px] rounded-2xl shadow-lg -order-1 md:order-3 self-center m-4 md:m-0'/>
