ebfd2f1464b157c2513f35afa0fe127da1c13456
a-down <136649391+a2downing@users.noreply.github.com>
2023-10-23 12:55:10 -0500

    analytics again

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { Analytics } from '@vercel/analytics/react';
+import { inject } from '@vercel/analytics/react';
+
+inject();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <Analytics />
   </React.StrictMode>,
 )
