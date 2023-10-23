e8786ef77f0272cb24538b735be0444ca0d0ec27
a-down <136649391+a2downing@users.noreply.github.com>
2023-10-23 12:56:05 -0500

    analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { inject } from '@vercel/analytics/react';
+import { inject } from '@vercel/analytics';
 
 inject();
 
