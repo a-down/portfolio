30d44732068462daed59c65ac3592537c8a4db87
a-down <136649391+a2downing@users.noreply.github.com>
2023-12-29 11:42:11 -0600

    clean

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { QuickMeasure, Portfolio } from "./pages";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Portfolio />} />
+        <Route path="/quickmeasure" element={<QuickMeasure />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,38 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Portfolio, QuickMeasure } from './pages'
-import Root from './Root'
+import App from './App'
 import './index.css'
 import { inject } from '@vercel/analytics';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 inject();
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Portfolio />,
-    // children: [
-    //   {
-    //     path: "quickmeasure",
-    //     element: <QuickMeasure />,
-    //   },
-    //   {
-    //     path: "*",
-    //     element: <Portfolio />,
-    //   },
-    // ]
-  },
-  {
-    path: "quickmeasure",
-    element: <QuickMeasure />,
-  }
-
-]);
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <RouterProvider router={router} /> */}
-    <Root />
+    <App />
   </React.StrictMode>,
 )
