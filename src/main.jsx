07caf6de427cad84f154283b088dd8067a2cec03
a-down <136649391+a2downing@users.noreply.github.com>
2023-12-29 11:15:22 -0600

    properly implemented RouterProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { Portfolio, QuickMeasure } from './pages'
 import './index.css'
 import { inject } from '@vercel/analytics';
-
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 inject();
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Portfolio />,
+  },
+  {
+    path: "/quickmeasure",
+    element: <QuickMeasure />,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
