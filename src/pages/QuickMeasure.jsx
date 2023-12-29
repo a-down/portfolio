55aab87ae8375de437c08b2804ab403329b56d93
a-down <136649391+a2downing@users.noreply.github.com>
2023-12-29 16:06:29 -0600

    contact section and footer

--- src/pages/QuickMeasure.jsx
+++ src/pages/QuickMeasure.jsx
@@ -30,17 +30,20 @@ export default function QuickMeasure() {
   return (
     <>
       <div className="font-jost relative w-full ">
+
+        {/* HEADER AND HERO */}
         <div className="h-[100dvh] pt-8 pl-4 md:pl-16 lg:pl-24" style={heroStyle} >
+          {/* HEADER */}
           <div className=" flex justify-between pr-4 md:pr-16 lg:pr-24">
             <a href="/" className="transition-all hover:opacity-60 flex gap-2 items-center">
               <FiArrowLeft />alecdowning.com</a>
             <a href="https://apps.apple.com/us/app/quick-measure-gps/id6473677147" target="_blank" className="bg-black/90 backdrop-blur-xl hover:bg-black/75 transition-all rounded-2xl flex justify-between items-center gap-4 text-lg text-white font-medium px-4 py-2.5">
-
               <p className="">View on the Apple App Store</p>
               <SiAppstore className="w-8 h-8 "/>
             </a>
           </div>
 
+          {/* HERO */}
           <div className=" flex items-center">
             <div className="">
               <h1 className="font-bold text-[64px] leading-none">Easy Tools for a <span className="text-[96px] text-[#6DAB64] block">Quick Measure</span></h1>
@@ -50,6 +53,7 @@ export default function QuickMeasure() {
           </div>
         </div>
 
+        {/* FEATURES SECTION */}
         <div className="mt-16 px-4 md:px-16 lg:px-24 ">
           <h2 className="text-4xl font-semibold">Features</h2>
 
@@ -58,7 +62,6 @@ export default function QuickMeasure() {
               <div className="flex gap-4 justify-end">
                 <img src={satelliteIcon} className="h-[128px]"/>
               </div>
-
               <div className="h-full flex flex-col justify-end">
                 <h3 className="text-2xl font-medium text-green-700 mb-2">Measure with GPS</h3>
                 <p className=" leading-snug max-w-[200px] text-gray-800">Measure automatically or manually with your location</p>
@@ -68,13 +71,11 @@ export default function QuickMeasure() {
             <div className="col-span-2 h-[280px] bg-gray-200/40 shadow-sm rounded-3xl px-6 py-4 flex items-end">
               <div className="flex flex-wrap gap-2">
                 <h3 className="text-xl font-medium text-gray-700 w-full">Units</h3>
-
                 {units.map((measurement, index) => (
                   <div className="bg-green-600 rounded-full px-4 py-1 " key={index}>
                     <p className=" leading-snug text-green-100">{measurement}</p>
                   </div>
                 ))}
-
               </div>
             </div>
 
@@ -83,34 +84,46 @@ export default function QuickMeasure() {
             </div>
 
             <div className="col-span-2 h-[280px] bg-green-600 shadow-sm rounded-3xl px-6 py-4 flex justify-center items-center">
-              {/* <h3 className="text-2xl font-medium text-gray-800">Save</h3> */}
               <div className='flex flex-col items-center justify-center'>
-                <img src={walkingIcon} className="h-[200px]"/>
-                {/* <p className="leading-none text-center text-gray-700 absolute bottom-6">Measure Anywhere by Tapping</p> */}
+                <img src={walkingIcon} className="h-[200px] mr-4"/>
               </div>
             </div>
 
             <div className="col-span-4 h-[280px] bg-gray-200/40 shadow-sm rounded-3xl px-6 py-4 flex items-center justify-center">
-              {/* <img src= */}
               <p className="text-2xl font-semibold text-green-700 text-center max-w-[480px]">Quick Measure was built so you can quickly and easily measure area and distance. It only takes 2 taps and feet to start!</p>
             </div>
 
             <div className="col-span-2 h-[280px] bg-gray-950 shadow-sm rounded-3xl px-6 py-4 flex justify-center items-center relative">
-              {/* <h3 className="text-2xl font-medium text-gray-800">Save</h3> */}
               <div className='flex flex-col items-center justify-center'>
                 <img src={downloadIcon} className="mb-2 h-[200px]" />
                 <p className="leading-none text-center text-gray-300 absolute bottom-6">Save Measurements</p>
               </div>
             </div>
+
           </div>
         </div>
 
-        <div className="my-32 px-4 md:px-16 lg:px-24 ">
-          <a href="mailto: alec@alecdowning.com" className="text-4xl font-semibold">Contact Me</a>
+        {/* CONTACT SECTION */}
+        <div className="mt-32 mb-16 px-4 md:px-16 lg:px-24 ">
+          <h2 className="text-4xl font-semibold mb-6">Contact Me</h2>
 
-          
-
+          <p className="text-xl">
+            Please reach out at{' '}
+            <a href="mailto: alec@alecdowning.com" className="text-green-700 underline hover:text-green-500 transition-all">alec@alecdowning.com</a>
+            {' '} with any questions, concerns, or feedback. I'd love to hear from you!
+          </p>
         </div>
+
+        <footer className="w-full bg-green-600">
+          <div className=" flex justify-between px-4 md:px-16 lg:px-24 py-8">
+            <a href="/" className="transition-all hover:opacity-60 flex gap-2 items-center text-xl">
+              <FiArrowLeft />Return to alecdowning.com</a>
+            <a href="https://apps.apple.com/us/app/quick-measure-gps/id6473677147" target="_blank" className="bg-black/90 backdrop-blur-xl hover:bg-black/75 transition-all rounded-2xl flex justify-between items-center gap-4 text-xl text-white font-medium px-4 py-2.5">
+              <p className="">View on the Apple App Store</p>
+              <SiAppstore className="w-8 h-8 "/>
+            </a>
+          </div>
+        </footer>
 
 
       </div>
