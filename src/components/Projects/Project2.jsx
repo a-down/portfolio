2e3b33a6fa2ee9b0441bc13eca63e901babdf567
a-down <136649391+a2downing@users.noreply.github.com>
2024-01-18 14:56:39 -0600

    overlay on hover

--- src/components/Projects/Project2.jsx
+++ src/components/Projects/Project2.jsx
@@ -6,6 +6,7 @@ export default function Project({ project, stackArr, isDark }) {
   const [ repoLink, setRepoLink ] = useState({color: '#e2e8f0'})
   const [ liveLink, setLiveLink ] = useState({color: '#e2e8f0'})
   const [ extraLink, setExtraLink ] = useState({color: '#e2e8f0'})
+  const [ overlay, setOverlay ] = useState(false)
 
   function setRepoLinkColor(isActive){
     const newColor = (isActive) ? project.color : '#e2e8f0'
@@ -22,11 +23,26 @@ export default function Project({ project, stackArr, isDark }) {
     setExtraLink({...liveLink, color: newColor})
   }
 
+  function displayOverlay() {
+
+  }
+
   return (
-    <div className="" style={{backgroundColor: project.color}}>
-      <div className="w-full aspect-video bg-slate-50/20 flex justify-center items-center">
+    <a 
+      className="block hover:scale-[102%] cursor-pointer " 
+      style={{backgroundColor: project.color}} 
+      onMouseEnter={() => setOverlay(true)}
+      onMouseLeave={() => setOverlay(false)}>
+      <div className="w-full aspect-video bg-slate-50/20 flex justify-center items-center relative">
         <img src={project.phoneImg} className="h-[80%] rounded-lg shadow-2xl"/>
+
+        <div className="absolute bottom-12 left-12 p-4 rounded-lg" style={{backgroundColor: project.color}}>
+          <h5 className="text-4xl font-medium text-white">{project.title}</h5>
+          
+        </div>
+
+        {overlay && ( <div className={`h-full w-full absolute top-0 left-0 opacity-20`} style={{backgroundColor: project.color}}></div> )}
       </div>
-    </div>
+    </a>
   )
 }
