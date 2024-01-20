92efe49fd9f33dd63b9c436f2e4385dc9bf139b4
a-down <136649391+a2downing@users.noreply.github.com>
2024-01-20 16:09:56 -0600

    header update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { HiOutlineFaceSmile } from 'react-icons/hi2'
+import smileIcon from '../assets/smile.svg'
 import resume from '../assets/alec-downing-resume.pdf'
 import { FaGithubSquare, FaLinkedin, FaEnvelope, FaArrowDown } from 'react-icons/fa'
 
@@ -12,13 +12,15 @@ export default function Header() {
 
         <div className='h-full items-center flex justify-between w-full opacity-100'>
 
-          <HiOutlineFaceSmile className=' text-brand-400 text-3xl'/>
+          <a href='/' className="hover:scale-105">
+            <img src={smileIcon} className='w-8 h-8'/>
+          </a>
 
           <nav className=' text-base flex items-center gap-6 text-brand-400 font-semibold'>
-            <a href='/' className=' hover:underline transition-all'>Home</a>
-            <a href='/#about' className=' hover:underline transition-all'>About</a>
-            <a href='/projects' className=' hover:underline transition-all'>Projects</a>
-            <a href='/#contact' className=' hover:underline transition-all'>Contact</a>
+            <a href='/' className=' hover:scale-105 transition-all'>Home</a>
+            <a href='/#about' className=' hover:scale-105 transition-all'>About</a>
+            <a href='/projects' className=' hover:scale-105 transition-all'>Projects</a>
+            <a href='/#contact' className=' hover:scale-105 transition-all'>Contact</a>
               {/* <a href='mailto: alec@alecdowning.com' className=' text-brand-400/80 hover:text-brand-400/100'> */}
                 {/* <FaEnvelope className='w-8 h-8'/> */}
                 {/* <p className="hidden md:inline">alec@alecdowning.com</p> */}
@@ -31,7 +33,7 @@ export default function Header() {
               {/* <a href='https://www.github.com/a-down' target='_blank'>
                 <FaGithubSquare className='w-8 h-8 mt-[3px]  text-brand-400/80 hover:text-brand-400/100'/>
               </a> */}
-            <a href={resume} download='alec-downing-resume' className='bg-brand-400/80 py-2 px-4 rounded-md text-slate-50 font-semibold hover:bg-brand-300 transition-all'>Resume</a>
+            <a href={resume} download='alec-downing-resume' className='bg-brand-400/80 py-2 px-4 rounded-md text-slate-50 font-semibold hover:scale-105 transition-all'>Resume</a>
           </nav>
 
         </div>
