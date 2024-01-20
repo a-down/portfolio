8ea51018dd285ecacc761cb95d21f4e42a77764d
a-down <136649391+a2downing@users.noreply.github.com>
2024-01-20 14:34:46 -0600

    sizing update

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -6,10 +6,10 @@ import { projects } from '@/components/Projects/ProjectDescriptions'
 export default function Projects() {
 
   return (
-    <div className='bg-slate-100 flex flex-col'>
+    <div className='bg-slate-100 flex flex-col items-center'>
       <Header />
-      <div className="py-24 px-24">
-        <h1 className="text-8xl leadinig-none font-bold tracking-tight text-brand-900">Projects</h1>
+      <div className="py-24 px-24 w-full max-w-[1400px]">
+        <h1 className="w-full text-left text-8xl leading-none font-bold tracking-tight text-brand-900">Projects</h1>
       </div>
 
       <div className="w-full px-24 max-w-[1400px]">
