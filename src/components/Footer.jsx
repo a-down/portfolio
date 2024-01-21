1ba11a03fa60a6c26591aa30841c494a9268d47c
a-down <136649391+a2downing@users.noreply.github.com>
2024-01-20 18:50:44 -0600

    footer links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,16 +1,70 @@
 import StackDisplay from './StackDisplay/StackDisplay'
+import { HiOutlineExternalLink, HiOutlineMail } from 'react-icons/hi'
+import { projects } from '@/components/Projects/ProjectDescriptions'
+import { useEffect, useState } from 'react'
 
 
 export default function Footer() {
+  const [projectsArr, setProjectsArr] = useState([])
+
   const footerStack = [
     'React', 'Tailwind', 'JavaScript', 'Vite', 'Vercel', 
   ]
 
+  const linksLinks = {
+    title: 'Links',
+    links: [
+      {name: 'Home', link: '/'},
+      {name: 'About', link: '/about'},
+      {name: 'Projects', link: '/projects'},
+      {name: 'GitHub', link: 'https://github.com/a-down', isExternal: true, icon: <HiOutlineExternalLink/>}
+    ]
+  }
+
+  const contactLinks = {
+    title: 'Links',
+    links: [
+      {name: 'Form', link: '/contact'},
+      {name: 'Email', link: 'mailto: alec@alecdowning.com', icon: <HiOutlineMail/>},
+      {name: 'LinkedIn', link: 'https://linkedin.com/in/alec-downing/', isExternal: true, icon: <HiOutlineExternalLink/>}
+    ]
+  }
+
+  let projectsLinks = {
+    title: 'Projects',
+    links: projectsArr
+  }
+
+  useEffect(() => {
+    let projectsForState = []
+    projects.forEach(project => {
+      projectsForState.push({name: project.title, link: `/projects/${project.slug}`})
+    })
+    setProjectsArr(projectsForState)
+  }, [])
+
+  const LinkColumn = ({ column }) => {
+    return (
+      <div className='flex flex-col gap-2'>
+        <h5 className='text-brand-200 font-semibold'>{column.title}</h5>
+        {column.links.map((link, index) => {
+          return (
+            <a className='text-brand-100 hover:scale-[102%] flex gap-1 items-center' href={link.link} target={link.isExternal ? '_blank' : ''} key={index + link}>
+              {link.name}
+              {link.icon}
+            </a>
+          )
+        })}
+      </div>
+    )
+  }
+
   return (
-    <footer className=" bg-brand-950 w-full flex justify-center py-4">
-      <div className='flex flex-col px-6 md:px-10 lg:px-24  gap-2'>
-        <h5 className='w-full text-slate-500 text-center select-none'>alecdowning.com was built with</h5>
-        <StackDisplay stackArr={footerStack} isDark={true}/>
+    <footer className=" bg-brand-950 w-full py-12">
+      <div className="max-w-[1400px] px-24 flex gap-12">
+        <LinkColumn column={linksLinks}/>
+        <LinkColumn column={contactLinks}/>
+        <LinkColumn column={projectsLinks}/>
       </div>
     </footer>
   )
