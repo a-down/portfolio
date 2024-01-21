eaca6e44459984f92d138d7356537f53947dbb2c
a-down <136649391+a2downing@users.noreply.github.com>
2024-01-20 20:55:27 -0600

    quick measure images

--- src/components/Projects/ProjectDescriptions.js
+++ src/components/Projects/ProjectDescriptions.js
@@ -1,12 +1,6 @@
-
-import techBlogPhone from '../../assets/project-visuals/tech-blog-phone.png'
-import techBlogBrowser from '../../assets/project-visuals/tech-blog-browser.png'
-import weatherDashboardPhone from '../../assets/project-visuals/weather-dashboard-phone.png'
-import weatherDashboardBrowser from '../../assets/project-visuals/weather-dashboard-browser.png'
-import noteTakerPhone from '../../assets/project-visuals/note-taker-phone.png'
-import noteTakerBrowser from '../../assets/project-visuals/note-taker-browser.png'
 import friendsDbPhone from '../../assets/project-visuals/friends-db-phone.png'
 import friendsDbBrowser from '../../assets/project-visuals/friends-db-browser.png'
+
 import gameWorldPhone from '../../assets/project-visuals/game-world-phone.png'
 import gameWorldBrowser from '../../assets/project-visuals/game-world-browser.png'
 
@@ -14,6 +8,8 @@ import quickMeasureAuto from '../../assets/project-visuals/quick-measure-auto.pn
 import quickMeasureTap from '../../assets/project-visuals/quick-measure-tap.png'
 import quickMeasureSaving from '../../assets/project-visuals/quick-measure-saving.png'
 import quickMeasureHome from '../../assets/project-visuals/quick-measure-home.png'
+import quickMeasureBrowser1 from '../../assets/project-visuals/quick-measure-browser-1.png'
+import quickMeasureBrowser2 from '../../assets/project-visuals/quick-measure-browser-2.png'
 
 import jobNotebookBrowser1 from '../../assets/project-visuals/job-notebook-browser-1.png'
 import jobNotebookBrowser2 from '../../assets/project-visuals/job-notebook-browser-2.png'
@@ -47,7 +43,9 @@ export let projects = [
       { img: quickMeasureHome, type: 'phone' },
       { img: quickMeasureAuto, type: 'phone' },
       { img: quickMeasureTap, type: 'phone' },
-      { img: quickMeasureSaving, type: 'phone' }
+      { img: quickMeasureSaving, type: 'phone' },
+      { img: quickMeasureBrowser1, type: 'browser' },
+      { img: quickMeasureBrowser2, type: 'browser' }
     ],
     repoLink: 'https://github.com/a-down/quick-measure-gps-app',
     deployedLink: '/quickmeasure',
@@ -166,29 +164,5 @@ export let projects = [
   //   description: 'WeatherCraft is a weather site built with jQuery (JavaScript), HTML, vanilla CSS, and OpenWeatherMap Weather API. Users can view weather by searching a city or zip code and country. The current location will display current weather, upcoming weather for the night, and an bi-hourly forecast. Users can set a favorite location that defaults on site load, as well as view their last five searches in the search modal. Both pieces of information are stored in local storage.',
   //   description2: 'WeatherCraft is a rebuild of a project from my Full-Stack Certificate Program. Updates that I made included: building a backend to handle muptiple API calls, designing from scratch using a 4pt grid system and Figma, adding search by zip functionality, appended and removed elements dynamically, utilized href for searches to allow reloads/bookmarks, allowed users to set a favorite/default location',
   //   techStack: ['HTML', 'CSS', 'JavaScript', 'jQuery', 'Node.js', 'Express.js', 'GitHub', 'Render']
-  // },
-  // {
-  //   title: 'Undefined Opinions',
-  //   color: '#3E829A',
-  //   phoneImg: techBlogPhone,
-  //   browserImg: techBlogBrowser,
-  //   // cardOrder: [3, 2, 1],
-  //   repoLink: 'https://github.com/a-down/full-stack-tech-blog',
-  //   deployedLink: 'https://a-down-tech-blog-b6cbf85bb984.herokuapp.com',
-  //   description: 'Undefined Opinions is a full-stack website that allows users to view blog posts posted on the site. Users can also create an account, post and edit their own blog posts, and comment on posts made by others. Users must be signed in to add posts or comments to the MySQL database.',
-  //   description2: 'The blog side uses server-side rendering (Handlebars) to update the site based on database data and the logged-in user.',
-  //   techStack: ['HTML', 'CSS', 'JavaScript', 'Bulma', 'Handlebars', 'jQuery', 'Node.js', 'Express.js', 'MySQL', 'Sequelize', 'GitHub', 'Heroku']
-  // },
-  // {
-  //   title: 'Note Taker',
-  //   color: '#44CAF0',
-  //   phoneImg: noteTakerPhone,
-  //   browserImg: noteTakerBrowser,
-  //   // cardOrder: [3, 2, 1],
-  //   repoLink: 'https://github.com/a-down/note-taker',
-  //   deployedLink: 'https://a-down-note-taker-a4f5d9c37c25.herokuapp.com/',
-  //   description: 'Note Taker is a full-stack website for users to save notes to stay organized. Express.js and Node.js save the notes as JSON files in the server. Users can also delete notes saved to the site.',
-  //   description2: 'I built out the backend of the website and deployed the website to Heroku.',
-  //   techStack: ['HTML', 'CSS', 'JavaScript', 'Node.js', 'Express.js', 'GitHub', 'Heroku']
-  // },
+  // }
 ]
