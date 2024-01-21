a18a1e41b88b29be3927c9c4961d310afe562bf7
a-down <136649391+a2downing@users.noreply.github.com>
2024-01-21 14:24:08 -0600

    responsive header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,40 @@
 import smileIcon from '../assets/smile.svg'
 import resume from '../assets/alec-downing-resume.pdf'
+import { HiMenu } from 'react-icons/hi'
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
 
 
 export default function Header({ location }) {
 
+  const Navigation = () => (
+    <nav className=' text-base flex flex-col md:flex-row items-start md:items-center gap-8 md:gap-6 text-brand-400 font-semibold'>
+      <a href='/' className={` hover:scale-105 transition-all active:text-brand-800 text-brand-${location === 'home' ? '800' : '400'}`}>
+        Home
+      </a>
+      <a href='/about' className=' hover:scale-105 transition-all active:text-brand-800'>
+        About
+      </a>
+      <a href='/projects' className={` hover:scale-105 transition-all active:text-brand-800 text-brand-${location === 'projects' ? '800' : '400'}`}>
+        Projects
+      </a>
+      <a href='/contact' className=' hover:scale-105 transition-all active:text-brand-800'>
+        Contact
+      </a>
+
+      <a href={resume} download='alec-downing-resume' className='bg-brand-400/80 py-2 px-4 rounded-md text-slate-50 font-semibold hover:scale-105 active:bg-brand-400 transition-all'>Resume</a>
+    </nav>
+  )
+
 
   return (
-    <div className="max-w-[1400px] mx-auto px-24 w-full">
+    <div className="max-w-[1400px] mx-auto px-8 md:px-12 lg:px-24 w-full">
       <header className=' w-full h-[100px]'>
 
         <div className='h-full items-center flex justify-between w-full opacity-100'>
@@ -15,22 +43,23 @@ export default function Header({ location }) {
             <img src={smileIcon} className='w-8 h-8'/>
           </a>
 
-          <nav className=' text-base flex items-center gap-6 text-brand-400 font-semibold'>
-            <a href='/' className={` hover:scale-105 transition-all active:text-brand-800 text-brand-${location === 'home' ? '800' : '400'}`}>
-              Home
-            </a>
-            <a href='/about' className=' hover:scale-105 transition-all active:text-brand-800'>
-              About
-            </a>
-            <a href='/projects' className={` hover:scale-105 transition-all active:text-brand-800 text-brand-${location === 'projects' ? '800' : '400'}`}>
-              Projects
-            </a>
-            <a href='/contact' className=' hover:scale-105 transition-all active:text-brand-800'>
-              Contact
-            </a>
+          <span className="hidden md:flex">
+            <Navigation/>
+          </span>
 
-            <a href={resume} download='alec-downing-resume' className='bg-brand-400/80 py-2 px-4 rounded-md text-slate-50 font-semibold hover:scale-105 active:bg-brand-400 transition-all'>Resume</a>
-          </nav>
+          <Sheet>
+            <SheetTrigger className="md:hidden">
+              <HiMenu className="text-2xl hover:scale-105 text-brand-800"/>
+            </SheetTrigger>
+            <SheetContent>
+              <SheetHeader>
+                <SheetTitle>
+                  <img src={smileIcon} className='w-8 h-8 mb-4'/>
+                </SheetTitle>
+                <Navigation />
+              </SheetHeader>
+            </SheetContent>
+          </Sheet>
 
         </div>
 
