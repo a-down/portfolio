99879f391cc7b29231ba249b37c8ca499b5af3a6
a-down <alec@characterstrong.com>
2024-11-03 22:40:23 -0600

    setup initial partykit server

---
+++ src/pages/Partykit.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import PartySocket from "partysocket";
+
+export default function Partykit() {
+  const [messages, setMessages] = useState([]);
+  const [input, setInput] = useState("");
+
+  // connect to our server
+  const partySocket = new PartySocket({
+    host: "localhost:1999",
+    room: "my-room",
+  });
+
+  // send a message to the server
+  // partySocket.send("Hello everyone");
+
+  // print each incoming message from the server to console
+  partySocket.addEventListener("message", (e) => {
+    const newMessages = [...messages];
+    if (messages[messages.length - 1] !== e.data) {
+      newMessages.push(e.data);
+      setMessages(newMessages);
+    }
+  });
+
+  return (
+    <div className="bg-slate-100">
+      <h1>Partykit</h1>
+      {/* <h1 onClick={() => partySocket.send("hello")}>Partykit</h1> */}
+      <input type="text" onChange={(e) => setInput(e.target.value)} />
+      <button onClick={() => partySocket.send(input)}>Send</button>
+      <div>
+        {messages.map((message, index) => (
+          <div key={index}>{message}</div>
+        ))}
+      </div>
+    </div>
+  );
+}
